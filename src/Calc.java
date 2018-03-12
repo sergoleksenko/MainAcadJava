a8ae2558ebf3160aef49e6100d5918d87adb8b97
Sergey Oleksenko <serge.oleksenko@jobjet.com>
2018-03-12 12:00:24 +0200

    added classes and .gitignore file

---
+++ src/Calc.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Serg on 3/12/18.
+ */
+public class Calc {
+}
---
+++ src/MainAcadJava.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Serg on 3/12/18.
+ */
+public class MainAcadJava {
+    public static void main(String[] arg) {
+
+    }
+}
