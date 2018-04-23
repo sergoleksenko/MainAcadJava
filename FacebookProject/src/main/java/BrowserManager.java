7c659b6239a8a12ae14a89de2a49afcf7788b02f
Sergey Oleksenko <serge.oleksenko@jobjet.com>
2018-04-23 15:57:54 +0300

    added logger into Facebook project

--- FacebookProject/src/main/java/BrowserManager.java
+++ FacebookProject/src/main/java/BrowserManager.java
@@ -1,3 +1,6 @@
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
@@ -9,9 +12,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BrowserManager {
 
     public static WebDriver browser;
+    private static Logger logger = LogManager.getLogger(BrowserManager.class);
+
 
     public static void open() {
         String chromeDriver;
+        PropertyConfigurator.configure("log4j.properties");
 
         if (System.getProperty("os.name").equals("Mac OS X")) {
             chromeDriver = "chromedriver";
@@ -19,6 +25,7 @@ public class BrowserManager {
             chromeDriver = "chromedriver.exe";
         }
 
+        logger.info("open browser");
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/" + chromeDriver);
         browser = new ChromeDriver();
         maximizeBrowser();
@@ -26,15 +33,18 @@ public class BrowserManager {
 
     public static void close() {
         if (browser != null)
+            logger.info("close browser");
             browser.quit();
     }
 
     private static void maximizeBrowser() {
+        logger.info("maximize browser");
         browser.manage().window().setPosition(new Point(0, 0));
         browser.manage().window().setSize(new Dimension(1920, 1080));
     }
 
     public static FacebookStartPage openFacebook() {
+        logger.info("open Facebook");
         browser.get("https://www.facebook.com");
 
         return new FacebookStartPage();
